9817bf715282656f145f2e72ca452fe969441879
Nguyễn Thanh Vũ <nthanhvu1411@gmail.com>
2026-08-13 20:31:20 +0700

    test(lesson): scaffold admin closure integration tests

---
+++ HanYu-Web/Tests/HanYu.IntegrationTests/Lesson/LessonAdminClosureIntegrationTests.cs
@@ -0,0 +1,8 @@
+namespace HanYu.IntegrationTests.Lesson;
+
+using Common;
+
+public sealed class LessonAdminClosureIntegrationTests : IntegrationTestBase
+{
+    public LessonAdminClosureIntegrationTests(HanYuWebApplicationFactory factory) : base(factory) { }
+}
