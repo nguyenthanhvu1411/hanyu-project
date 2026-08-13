c5de52926b56f76ec61eb6c36b0dc9966eeeef75
Nguyễn Thanh Vũ <nthanhvu1411@gmail.com>
2026-08-13 20:34:41 +0700

    chore(tests): remove empty lesson admin closure scaffold

--- HanYu-Web/Tests/HanYu.IntegrationTests/Lesson/LessonAdminClosureIntegrationTests.cs
+++
@@ -1,8 +0,0 @@
-namespace HanYu.IntegrationTests.Lesson;
-
-using Common;
-
-public sealed class LessonAdminClosureIntegrationTests : IntegrationTestBase
-{
-    public LessonAdminClosureIntegrationTests(HanYuWebApplicationFactory factory) : base(factory) { }
-}
