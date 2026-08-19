b8c5f6079780bc51b3bc9c0545b5d5f499c3ce94
Nguyễn Thanh Vũ <nthanhvu1411@gmail.com>
2026-08-19 23:15:34 +0700

    feat: consume quiz membership read endpoints

--- admin-web/src/features/quiz/quiz.api.ts
+++ admin-web/src/features/quiz/quiz.api.ts
@@ -35,168 +35,52 @@ export const quizApi = {
     const queryString = buildQuery(query);
     return apiClient<PagedResult<AdminQuiz>>(queryString ? `${ROOT}?${queryString}` : ROOT);
   },
+  getById(id: number) { return apiClient<AdminQuiz>(`${ROOT}/${id}`); },
+  create(request: CreateQuizRequest) { return apiClient<AdminQuiz>(ROOT, { method: "POST", body: request }); },
+  update(id: number, request: UpdateQuizRequest) { return apiClient<AdminQuiz>(`${ROOT}/${id}`, { method: "PUT", body: request }); },
+  submitReview(id: number) { return apiClient<void>(`${ROOT}/${id}/submit-review`, { method: "POST" }); },
+  approve(id: number) { return apiClient<void>(`${ROOT}/${id}/approve`, { method: "POST" }); },
+  publish(id: number) { return apiClient<void>(`${ROOT}/${id}/publish`, { method: "POST" }); },
+  archive(id: number) { return apiClient<void>(`${ROOT}/${id}/archive`, { method: "POST" }); },
+  restore(id: number) { return apiClient<void>(`${ROOT}/${id}/restore`, { method: "POST" }); },
+  delete(id: number) { return apiClient<void>(`${ROOT}/${id}`, { method: "DELETE" }); },
 
-  getById(id: number) {
-    return apiClient<AdminQuiz>(`${ROOT}/${id}`);
-  },
+  listQuestions(quizId: number) { return apiClient<AdminQuizQuestion[]>(`${ROOT}/${quizId}/questions`); },
+  createQuestion(quizId: number, request: QuizQuestionRequest) { return apiClient<AdminQuizQuestion>(`${ROOT}/${quizId}/questions`, { method: "POST", body: request }); },
+  updateQuestion(quizId: number, questionId: number, request: QuizQuestionRequest) { return apiClient<AdminQuizQuestion>(`${ROOT}/${quizId}/questions/${questionId}`, { method: "PUT", body: request }); },
+  deleteQuestion(quizId: number, questionId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}`, { method: "DELETE" }); },
+  submitQuestionReview(quizId: number, questionId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/submit-review`, { method: "POST" }); },
+  approveQuestion(quizId: number, questionId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/approve`, { method: "POST" }); },
+  publishQuestion(quizId: number, questionId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/publish`, { method: "POST" }); },
+  archiveQuestion(quizId: number, questionId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/archive`, { method: "POST" }); },
+  restoreQuestion(quizId: number, questionId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/restore`, { method: "POST" }); },
 
-  create(request: CreateQuizRequest) {
-    return apiClient<AdminQuiz>(ROOT, { method: "POST", body: request });
-  },
+  listQuestionOptions(quizId: number, questionId: number) { return apiClient<AdminQuizQuestionOption[]>(`${ROOT}/${quizId}/questions/${questionId}/options`); },
+  createQuestionOption(quizId: number, questionId: number, request: QuizQuestionOptionRequest) { return apiClient<AdminQuizQuestionOption>(`${ROOT}/${quizId}/questions/${questionId}/options`, { method: "POST", body: request }); },
+  updateQuestionOption(quizId: number, questionId: number, optionId: number, request: QuizQuestionOptionRequest) { return apiClient<AdminQuizQuestionOption>(`${ROOT}/${quizId}/questions/${questionId}/options/${optionId}`, { method: "PUT", body: request }); },
+  deleteQuestionOption(quizId: number, questionId: number, optionId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/options/${optionId}`, { method: "DELETE" }); },
 
-  update(id: number, request: UpdateQuizRequest) {
-    return apiClient<AdminQuiz>(`${ROOT}/${id}`, { method: "PUT", body: request });
-  },
+  listMatchingPairs(quizId: number, questionId: number) { return apiClient<AdminQuizMatchingPair[]>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs`); },
+  createMatchingPair(quizId: number, questionId: number, request: QuizMatchingPairRequest) { return apiClient<AdminQuizMatchingPair>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs`, { method: "POST", body: request }); },
+  updateMatchingPair(quizId: number, questionId: number, pairId: number, request: QuizMatchingPairRequest) { return apiClient<AdminQuizMatchingPair>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs/${pairId}`, { method: "PUT", body: request }); },
+  deleteMatchingPair(quizId: number, questionId: number, pairId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs/${pairId}`, { method: "DELETE" }); },
 
-  submitReview(id: number) {
-    return apiClient<void>(`${ROOT}/${id}/submit-review`, { method: "POST" });
-  },
+  listTags() { return apiClient<AdminQuizTag[]>(TAG_ROOT); },
+  createTag(request: QuizTagRequest) { return apiClient<AdminQuizTag>(TAG_ROOT, { method: "POST", body: request }); },
+  updateTag(id: number, request: QuizTagRequest) { return apiClient<AdminQuizTag>(`${TAG_ROOT}/${id}`, { method: "PUT", body: request }); },
+  activateTag(id: number) { return apiClient<void>(`${TAG_ROOT}/${id}/activate`, { method: "POST" }); },
+  deactivateTag(id: number) { return apiClient<void>(`${TAG_ROOT}/${id}/deactivate`, { method: "POST" }); },
+  deleteTag(id: number) { return apiClient<void>(`${TAG_ROOT}/${id}`, { method: "DELETE" }); },
+  listQuestionTags(quizId: number, questionId: number) { return apiClient<AdminQuizTag[]>(`${ROOT}/${quizId}/questions/${questionId}/tags`); },
+  attachTag(quizId: number, questionId: number, tagId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/tags/${tagId}`, { method: "POST" }); },
+  detachTag(quizId: number, questionId: number, tagId: number) { return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/tags/${tagId}`, { method: "DELETE" }); },
 
-  approve(id: number) {
-    return apiClient<void>(`${ROOT}/${id}/approve`, { method: "POST" });
-  },
-
-  publish(id: number) {
-    return apiClient<void>(`${ROOT}/${id}/publish`, { method: "POST" });
-  },
-
-  archive(id: number) {
-    return apiClient<void>(`${ROOT}/${id}/archive`, { method: "POST" });
-  },
-
-  restore(id: number) {
-    return apiClient<void>(`${ROOT}/${id}/restore`, { method: "POST" });
-  },
-
-  delete(id: number) {
-    return apiClient<void>(`${ROOT}/${id}`, { method: "DELETE" });
-  },
-
-  listQuestions(quizId: number) {
-    return apiClient<AdminQuizQuestion[]>(`${ROOT}/${quizId}/questions`);
-  },
-
-  createQuestion(quizId: number, request: QuizQuestionRequest) {
-    return apiClient<AdminQuizQuestion>(`${ROOT}/${quizId}/questions`, { method: "POST", body: request });
-  },
-
-  updateQuestion(quizId: number, questionId: number, request: QuizQuestionRequest) {
-    return apiClient<AdminQuizQuestion>(`${ROOT}/${quizId}/questions/${questionId}`, { method: "PUT", body: request });
-  },
-
-  deleteQuestion(quizId: number, questionId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}`, { method: "DELETE" });
-  },
-
-  submitQuestionReview(quizId: number, questionId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/submit-review`, { method: "POST" });
-  },
-
-  approveQuestion(quizId: number, questionId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/approve`, { method: "POST" });
-  },
-
-  publishQuestion(quizId: number, questionId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/publish`, { method: "POST" });
-  },
-
-  archiveQuestion(quizId: number, questionId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/archive`, { method: "POST" });
-  },
-
-  restoreQuestion(quizId: number, questionId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/restore`, { method: "POST" });
-  },
-
-  listQuestionOptions(quizId: number, questionId: number) {
-    return apiClient<AdminQuizQuestionOption[]>(`${ROOT}/${quizId}/questions/${questionId}/options`);
-  },
-
-  createQuestionOption(quizId: number, questionId: number, request: QuizQuestionOptionRequest) {
-    return apiClient<AdminQuizQuestionOption>(`${ROOT}/${quizId}/questions/${questionId}/options`, { method: "POST", body: request });
-  },
-
-  updateQuestionOption(quizId: number, questionId: number, optionId: number, request: QuizQuestionOptionRequest) {
-    return apiClient<AdminQuizQuestionOption>(`${ROOT}/${quizId}/questions/${questionId}/options/${optionId}`, { method: "PUT", body: request });
-  },
-
-  deleteQuestionOption(quizId: number, questionId: number, optionId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/options/${optionId}`, { method: "DELETE" });
-  },
-
-  listMatchingPairs(quizId: number, questionId: number) {
-    return apiClient<AdminQuizMatchingPair[]>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs`);
-  },
-
-  createMatchingPair(quizId: number, questionId: number, request: QuizMatchingPairRequest) {
-    return apiClient<AdminQuizMatchingPair>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs`, { method: "POST", body: request });
-  },
-
-  updateMatchingPair(quizId: number, questionId: number, pairId: number, request: QuizMatchingPairRequest) {
-    return apiClient<AdminQuizMatchingPair>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs/${pairId}`, { method: "PUT", body: request });
-  },
-
-  deleteMatchingPair(quizId: number, questionId: number, pairId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/matching-pairs/${pairId}`, { method: "DELETE" });
-  },
-
-  listTags() {
-    return apiClient<AdminQuizTag[]>(TAG_ROOT);
-  },
-
-  createTag(request: QuizTagRequest) {
-    return apiClient<AdminQuizTag>(TAG_ROOT, { method: "POST", body: request });
-  },
-
-  updateTag(id: number, request: QuizTagRequest) {
-    return apiClient<AdminQuizTag>(`${TAG_ROOT}/${id}`, { method: "PUT", body: request });
-  },
-
-  activateTag(id: number) {
-    return apiClient<void>(`${TAG_ROOT}/${id}/activate`, { method: "POST" });
-  },
-
-  deactivateTag(id: number) {
-    return apiClient<void>(`${TAG_ROOT}/${id}/deactivate`, { method: "POST" });
-  },
-
-  deleteTag(id: number) {
-    return apiClient<void>(`${TAG_ROOT}/${id}`, { method: "DELETE" });
-  },
-
-  attachTag(quizId: number, questionId: number, tagId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/tags/${tagId}`, { method: "POST" });
-  },
-
-  detachTag(quizId: number, questionId: number, tagId: number) {
-    return apiClient<void>(`${ROOT}/${quizId}/questions/${questionId}/tags/${tagId}`, { method: "DELETE" });
-  },
-
-  listQuestionBanks() {
-    return apiClient<AdminQuestionBank[]>(QUESTION_BANK_ROOT);
-  },
-
-  createQuestionBank(request: QuestionBankRequest) {
-    return apiClient<AdminQuestionBank>(QUESTION_BANK_ROOT, { method: "POST", body: request });
-  },
-
-  updateQuestionBank(id: number, request: QuestionBankRequest) {
-    return apiClient<AdminQuestionBank>(`${QUESTION_BANK_ROOT}/${id}`, { method: "PUT", body: request });
-  },
-
-  activateQuestionBank(id: number) {
-    return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/activate`, { method: "POST" });
-  },
-
-  deactivateQuestionBank(id: number) {
-    return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/deactivate`, { method: "POST" });
-  },
-
-  addQuestionToBank(id: number, questionId: number) {
-    return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/questions`, { method: "POST", body: { questionId } });
-  },
-
-  removeQuestionFromBank(id: number, questionId: number) {
-    return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/questions/${questionId}`, { method: "DELETE" });
-  },
+  listQuestionBanks() { return apiClient<AdminQuestionBank[]>(QUESTION_BANK_ROOT); },
+  createQuestionBank(request: QuestionBankRequest) { return apiClient<AdminQuestionBank>(QUESTION_BANK_ROOT, { method: "POST", body: request }); },
+  updateQuestionBank(id: number, request: QuestionBankRequest) { return apiClient<AdminQuestionBank>(`${QUESTION_BANK_ROOT}/${id}`, { method: "PUT", body: request }); },
+  activateQuestionBank(id: number) { return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/activate`, { method: "POST" }); },
+  deactivateQuestionBank(id: number) { return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/deactivate`, { method: "POST" }); },
+  listQuestionBankQuestions(id: number) { return apiClient<AdminQuizQuestion[]>(`${QUESTION_BANK_ROOT}/${id}/questions`); },
+  addQuestionToBank(id: number, questionId: number) { return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/questions`, { method: "POST", body: { questionId } }); },
+  removeQuestionFromBank(id: number, questionId: number) { return apiClient<void>(`${QUESTION_BANK_ROOT}/${id}/questions/${questionId}`, { method: "DELETE" }); },
 };
